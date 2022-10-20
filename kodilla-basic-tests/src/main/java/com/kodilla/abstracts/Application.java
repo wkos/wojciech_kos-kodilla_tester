18efe7250f82f2ffac6cd847733f2599e4b79179
Wojciech Kos <obohp@wp.pl>
2022-10-20 19:30:31 +0200

    Abstract classes

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/Application.java
@@ -0,0 +1,13 @@
+package com.kodilla.abstracts;
+
+public class Application {
+    public static void main(String[] args) {
+//        Dog dog = new Dog();
+//        dog.giveVoice();
+//        Duck duck = new Duck();
+//        duck.giveVoice();
+        Animal dog = new Dog();
+        AnimalProcessor processor = new AnimalProcessor();
+        processor.process(dog);
+    }
+}
