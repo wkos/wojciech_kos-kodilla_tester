e72af82426a11a4b1190f8d9ac0309064827dd72
Wojciech Kos <obohp@wp.pl>
2022-10-21 15:04:32 +0200

    Calculator tests

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -12,5 +12,22 @@ public class Application {
         } else {
             System.out.println("Metoda add nie działa poprawnie dla liczb " + a + " i " + b);
         }
+
+        int subtractResult = calculator.subtract(a, b);
+        correct = ResultChecker.assertEquals(2, subtractResult);
+        if(correct){
+            System.out.println("Metoda subtract działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda subtract nie działa poprawnie dla liczb " + a + " i " + b);
+        }
+
+        int squareResult = calculator.square(a);
+        correct = ResultChecker.assertEquals(49, squareResult);
+        if(correct){
+            System.out.println("Metoda square działa poprawnie dla liczby " + a);
+        } else {
+            System.out.println("Metoda square nie działa poprawnie dla liczby " + a);
+        }
     }
+
 }
--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -1,11 +1,15 @@
 package com.kodilla.basic_assertion;
 
 public class Calculator {
-    public  int add(int numberOne, int numberTwo){
+    public int add(int numberOne, int numberTwo) {
         return numberOne + numberTwo;
     }
 
-    public  int subtract(int numberOne, int numberTwo){
+    public int subtract(int numberOne, int numberTwo) {
         return numberOne - numberTwo;
+    }
+
+    public int square(int number){
+        return number * number;
     }
 }
