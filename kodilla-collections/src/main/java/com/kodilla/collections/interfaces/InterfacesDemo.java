65e0e633ed14e54042f4c7cedabdebce5cfb188e
Wojciech Kos <obohp@wp.pl>
2022-11-11 10:02:41 +0100

    Example interfaces

---
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/InterfacesDemo.java
@@ -0,0 +1,40 @@
+package com.kodilla.collections.interfaces;
+
+public class InterfacesDemo {
+    public static void main(String[] args) {
+        Square square = new Square(10.0);
+//        showSquareDetails(square);
+        showShapeDetails(square);
+
+        Circle circle = new Circle(7.0);
+//        showCircleDetails(circle);
+        showShapeDetails(circle);
+
+        Triangle triangle = new Triangle(6.0, 4.0, 7.211);
+//        showTriangleDetails(triangle);
+        showShapeDetails(triangle);
+    }
+
+
+
+//    private static void showTriangleDetails(Triangle triangle) {
+//        System.out.println(triangle.getArea());
+//        System.out.println(triangle.getPerimeter());
+//    }
+//
+//    private static void showCircleDetails(Circle circle) {
+//        System.out.println(circle.getArea());
+//        System.out.println(circle.getPerimeter());
+//    }
+//
+//
+//    private static void showSquareDetails(Square square) {
+//        System.out.println(square.getArea());
+//        System.out.println(square.getPerimeter());
+//    }
+
+    private static void showShapeDetails(Shape shape) {
+        System.out.println(shape.getArea());
+        System.out.println(shape.getPerimeter());
+    }
+}
