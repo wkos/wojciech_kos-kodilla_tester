3d1b5b567c80d146bcffd97c5c11c96fcda8a248
Wojciech Kos <obohp@wp.pl>
2022-11-27 11:29:55 +0100

    Exceptions - examples

---
+++ kodilla-stream/src/main/java/com/kodilla/exception/FirstExample.java
@@ -0,0 +1,21 @@
+package com.kodilla.exception;
+
+public class FirstExample {
+    public static void main(String[] args) {
+//        String username = "Willy Wonka";
+//        String[] nameParts = username.split(" ");
+//        System.out.println("First name: " + nameParts[0]);
+//        System.out.println("Last name: " + nameParts[1]);
+
+
+//        String username = null;
+//        String[] nameParts = username.split(" ");
+//        System.out.println("First name: " + nameParts[0]);
+//        System.out.println("Last name: " + nameParts[1]);
+
+        String username = "Willy";
+        String[] nameParts = username.split(" ");
+        System.out.println("First name: " + nameParts[0]);
+        System.out.println("Last name: " + nameParts[1]);
+    }
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/exception/ThirdExample.java
@@ -0,0 +1,16 @@
+package com.kodilla.exception;
+
+public class ThirdExample {
+    public static void main(String[] args) {
+        AirportRepository airportRepository = new AirportRepository();
+        boolean isViennaInUse = false;
+        try {
+            isViennaInUse = airportRepository.isAirportInUse("Vienna");
+            System.out.println("Vienna status: " + isViennaInUse);
+        } catch (AirportNotFoundException e) {
+            System.out.println("Vienna is not in use");
+        } finally {
+            System.out.println("Thank you for using Kodilla airlines");
+        }
+    }
+}
