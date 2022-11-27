a699d3d1ac5cfdbd23b128cdba030081f05b635a
Wojciech Kos <obohp@wp.pl>
2022-11-27 17:32:44 +0100

    assertThrows - example

---
+++ kodilla-stream/src/test/java/com/kodilla/exception/AirportRepositoryTestSuite.java
@@ -0,0 +1,27 @@
+package com.kodilla.exception;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AirportRepositoryTestSuite {
+    @Test
+    public void testIsAirportInUse() throws AirportNotFoundException{
+        //given
+        AirportRepository airportRepository = new AirportRepository();
+        //when
+        boolean isWarsawInUse = airportRepository.isAirportInUse("Warsaw");
+        //then
+        assertTrue(isWarsawInUse);
+    }
+
+    @Test
+    public void testIsAirportInUseWithException(){
+        //given
+        AirportRepository airportRepository = new AirportRepository();
+        //when
+        //then
+        assertThrows(AirportNotFoundException.class, () -> airportRepository.isAirportInUse("Vienna"));
+    }
+}
