e7f827cc9bdaf1c07831658e90f5f3104b9899ce
Wojciech Kos <obohp@wp.pl>
2022-12-01 20:43:48 +0100

    Data from file

--- kodilla-advanced-tests/src/test/java/com/kodilla/parmetrized_tests/StringManipulatorTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parmetrized_tests/StringManipulatorTestSuite.java
@@ -1,6 +1,7 @@
 package com.kodilla.parmetrized_tests;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,6 +15,12 @@ class StringManipulatorTestSuite {
     }
 
     @ParameterizedTest
+    @CsvFileSource(resources = "/stringWithLowerCase.csv", numLinesToSkip = 1)
+    public void shouldReturnReverseStringWithLowerCaseFromFile(String input, String expected){
+        assertEquals(expected, stringManipulator.reverseWithLowerCase(input));
+    }
+
+    @ParameterizedTest
     @CsvSource(value = {"test, 4", "a a a, 3", "null, 4", "A, 1"})
     public void shouldReturnLengthOfStringWithoutSpaces(String text, int length){
         int textLength = stringManipulator.getStringLengthWithoutSpaces(text);
@@ -23,6 +30,12 @@ class StringManipulatorTestSuite {
     @ParameterizedTest
     @CsvSource(value = {"te,st:1", "a,a,a,a:3", "ala ma kota:0", "null:0", "A:0"}, delimiter = ':')
     public void shouldReturnNumberOfCommasInString(String input, int expected){
+        assertEquals(expected, stringManipulator.countNumberOfCommas(input));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/stringWithCommas.csv", delimiter = ':')
+    public void shouldReturnNumberOfCommasInStringFromFile(String input, int expected){
         assertEquals(expected, stringManipulator.countNumberOfCommas(input));
     }
 }
