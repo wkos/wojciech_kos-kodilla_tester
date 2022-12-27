2015d7150a851e3458a74a429810dcaa3f77c8f8
Wojciech Kos <obohp@wp.pl>
2022-12-27 12:57:45 +0100

    Bean injection - exercises

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
@@ -1,6 +1,7 @@
 package com.kodilla.spring.basic.spring_dependency_injection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -39,21 +40,35 @@ public class SimpleApplication {
 //    }
 
     // Wstrzykiwanie za pomocą settera
-    private SkypeMessageService messageService;
-
+//    private SkypeMessageService messageService;
+//
+//    @Autowired
+//    public void setMessageService(SkypeMessageService messageService) {
+//        this.messageService = messageService;
+//    }
+//
+//    public String processMessage(String message, String receiver){
+//        if(checkReceiver(receiver)){
+//            return this.messageService.send(message, receiver);
+//        }
+//        return null;
+//    }
+//
+//    private boolean checkReceiver(String receiver){
+//        return receiver != null && !receiver.isEmpty();
+//    }
     @Autowired
-    public void setMessageService(SkypeMessageService messageService) {
-        this.messageService = messageService;
-    }
+    @Qualifier(value = "skypeMessageService")
+    private MessageService messageService;
 
-    public String processMessage(String message, String receiver){
-        if(checkReceiver(receiver)){
+    public String processMessage(String message, String receiver) {
+        if (checkReceiver(receiver)) {
             return this.messageService.send(message, receiver);
         }
         return null;
     }
 
-    private boolean checkReceiver(String receiver){
+    private boolean checkReceiver(String receiver) {
         return receiver != null && !receiver.isEmpty();
     }
 }
