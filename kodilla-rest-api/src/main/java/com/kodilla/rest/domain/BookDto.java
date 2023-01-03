de82f1040e2e2307e79ffb2400016bc6f5d72e6e
Wojciech Kos <obohp@wp.pl>
2023-01-03 18:31:55 +0100

    REST API - exercises

---
+++ kodilla-rest-api/src/main/java/com/kodilla/rest/domain/BookDto.java
@@ -0,0 +1,19 @@
+package com.kodilla.rest.domain;
+
+public class BookDto {
+    private String title;
+    private String author;
+
+    public BookDto(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
