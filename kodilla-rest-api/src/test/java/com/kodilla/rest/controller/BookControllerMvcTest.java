48170ca2a415511985974c81915d736e3d2592d9
Wojciech Kos <obohp@wp.pl>
2023-01-10 08:56:13 +0100

    Book controller - homework - corrected

--- kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerMvcTest.java
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerMvcTest.java
@@ -1,9 +1,12 @@
 package com.kodilla.rest.controller;
 
+import com.google.gson.Gson;
 import com.kodilla.rest.domain.BookDto;
 import com.kodilla.rest.service.BookService;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -30,26 +33,29 @@ public class BookControllerMvcTest {
         List<BookDto> books = new ArrayList<>();
         books.add(new BookDto("Title1", "Author1"));
         books.add(new BookDto("Title2", "Author2"));
-//        Mockito.when(bookService.getBooks()).thenReturn(books);
+        Mockito.when(bookService.getBooks()).thenReturn(books);
 
         //when & then
         mockMvc.perform(MockMvcRequestBuilders
                         .get("/books")
                         .contentType(MediaType.APPLICATION_JSON)
                 )
-                .andExpect(MockMvcResultMatchers.status().is(200));
-//                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(2)));
+                .andExpect(MockMvcResultMatchers.status().is(200))
+                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(2)));
     }
 
     @Test
     public void shouldInvokeAddBook() throws Exception {
-//        BookDto bookDto = new BookDto("Title", "Author");
+        BookDto bookDto = new BookDto("Title", "Author");
+        Gson gson = new Gson();
+        String jsonObject = gson.toJson(bookDto);
 //        Mockito.doNothing().when(bookService).addBook(bookDto);
         mockMvc.perform(MockMvcRequestBuilders.post("/books")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\n \"title\": \"Title\", \n\"author\": \"Author\"\n}")
+                        .content(jsonObject)
                         .accept(MediaType.APPLICATION_JSON)
                 )
                 .andExpect(MockMvcResultMatchers.status().isOk());
+        Mockito.verify(bookService).addBook(bookDto);
     }
 }
