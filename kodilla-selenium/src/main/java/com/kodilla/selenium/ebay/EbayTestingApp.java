94903f187f1187057297b6d540fc5f05b45115e0
Wojciech Kos <obohp@wp.pl>
2023-01-30 16:23:59 +0100

    Ebay testing - homework

--- kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EbayTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EbayTestingApp.java
@@ -1,4 +1,19 @@
 package com.kodilla.selenium.ebay;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class EbayTestingApp {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\selenium-drivers\\chrome\\chromedriver2.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.ebay.com/");
+
+        WebElement inputField = driver.findElement(By.name("_nkw"));
+        inputField.sendKeys("Laptop");
+        inputField = driver.findElement(By.id("gh-btn"));
+        inputField.submit();
+    }
 }
