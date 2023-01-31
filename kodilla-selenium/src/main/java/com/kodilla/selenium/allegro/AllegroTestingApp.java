fad9f94cf94809bc897508e46cc5a6dd715c4d9b
Wojciech Kos <obohp@wp.pl>
2023-01-31 19:44:11 +0100

    Xpath Allegro - homework

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -11,18 +11,17 @@ public class AllegroTestingApp {
         System.setProperty("webdriver.chrome.driver", "C:\\selenium-drivers\\chrome\\chromedriver2.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://allegro.pl");
-        Thread.sleep(5000);
+
+        WebElement cookieElement = driver.findElement(By.xpath("//button[@data-role=\"accept-consent\"]"));
+        cookieElement.click();
 
         WebElement categoryElement = driver.findElement(By.xpath("//div[@class=\"mp7g_oh mr3m_1 s4kyg\"]/select"));
         Select categorySelect = new Select(categoryElement);
         categorySelect.selectByValue("/kategoria/elektronika");
-        WebElement inputElement = driver.findElement(By.xpath("//input[@class=\"msts_pt mgmw_wo mr3m_1 mli2_1 mjyo_6x mse2_40 mqu1_40 mp4t_0 m3h2_0 mryx_0 munh_0 mg9e_0 mj7a_0 mh36_0 mvrt_8 mlkp_6x mefy_5r mldj_0 mm2b_0 ozg4u mh85_0 _535b5_Y5CDc\"]"));
-//        WebElement searchElement = driver.findElement(By.xpath("//input[@name=\"string\"]"));
-        System.out.println(inputElement.getTagName());
-        System.out.println(inputElement.getSize());
-//        inputElement.click();
-        inputElement.submit();
+
+        WebElement inputElement = driver.findElement(By.xpath("//input[@name=\"string\"]"));
         inputElement.sendKeys("Mavic mini");
+
         WebElement searchButton = driver.findElement(By.xpath("//button[@data-role=\"search-button\"]"));
         searchButton.submit();
     }
