9f699073b09f00f82e792818fd7c632bb82f45f3
Wojciech Kos <obohp@wp.pl>
2023-02-08 17:20:14 +0100

    Code analyzing

---
+++ kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTest.java
@@ -0,0 +1,19 @@
+package com.kodilla.jacoco;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class YearTest {
+    @Test
+    public void shouldBeLeapYearIfDivisibleBy400() {
+        //given
+        Year year = new Year(1600);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertTrue(isLeapYear);
+    }
+}
