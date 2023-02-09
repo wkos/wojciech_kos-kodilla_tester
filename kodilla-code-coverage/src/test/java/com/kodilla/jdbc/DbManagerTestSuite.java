e7f99bbdb3e3175af7c585ecdca9cc66fc47b681
Wojciech Kos <obohp@wp.pl>
2023-02-09 16:56:27 +0100

    JDBC - SQL exercises

---
+++ kodilla-code-coverage/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -0,0 +1,115 @@
+package com.kodilla.jdbc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.AbstractMap;
+import java.util.List;
+
+class DbManagerTestSuite {
+    private static DbManager dbManager;
+
+    @BeforeAll
+    public static void setup() throws SQLException {
+        dbManager = DbManager.getInstance();
+    }
+
+    @Test
+    void testConnect() {
+        //Given
+        //When
+        //Then
+        Assertions.assertNotNull(dbManager.getConnection());
+    }
+
+//    @Test
+//    void testSelectUsers() throws SQLException {
+//        //Given
+//        //When
+//        String sqlQuery = "SELECT * FROM USERS";
+//        Statement statement = dbManager.getConnection().createStatement();
+//        ResultSet rs = statement.executeQuery(sqlQuery);
+//
+//        //Then
+//        int counter = 0;
+//        while(rs.next()) {
+//            System.out.println(rs.getInt("ID") + ", " +
+//                    rs.getString("FIRSTNAME") + ", " +
+//                    rs.getString("LASTNAME"));
+//            counter++;
+//        }
+//        rs.close();
+//        statement.close();
+//        Assertions.assertEquals(5, counter);
+//    }
+
+    @Test
+    void testSelectUsers() throws SQLException {
+        //Given
+        String countQuery = "SELECT COUNT(*) FROM USERS";
+        Statement statement = createStatement();
+        ResultSet rs = statement.executeQuery(countQuery);
+        int count = getRowsCount(rs);
+        insertUsers(statement);
+
+        //When
+        String sqlQuery = "SELECT * FROM USERS";
+        statement = createStatement();
+        rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = getResultsCount(rs);
+        int expected = count + 5;
+        Assertions.assertEquals(expected, counter);
+
+        rs.close();
+        statement.close();
+    }
+
+    private Statement createStatement() throws SQLException {
+        return dbManager.getConnection().createStatement();
+    }
+
+    private static final List<AbstractMap.SimpleEntry<String, String>> USERS = List.of(
+            new AbstractMap.SimpleEntry<>("Zara", "Ali"),
+            new AbstractMap.SimpleEntry<>("Otman", "Use"),
+            new AbstractMap.SimpleEntry<>("Mark", "Boq"),
+            new AbstractMap.SimpleEntry<>("Uli", "Wimer"),
+            new AbstractMap.SimpleEntry<>("Oli", "Kosiw")
+    );
+
+    private void insertUsers(Statement statement) throws SQLException {
+        for (AbstractMap.SimpleEntry<String, String> user : USERS) {
+            statement.executeUpdate(
+                    String.format("INSERT INTO USERS(FIRSTNAME, LASTNAME) VALUES ('%s', '%s')",
+                            user.getKey(),
+                            user.getValue()
+                    )
+            );
+        }
+    }
+
+    private static int getResultsCount(ResultSet rs) throws SQLException {
+        int counter = 0;
+        while(rs.next()) {
+            System.out.printf("%d, %s, %s%n",
+                    rs.getInt("ID"),
+                    rs.getString("FIRSTNAME"),
+                    rs.getString("LASTNAME"));
+            counter++;
+        }
+        return counter;
+    }
+
+    private static int getRowsCount(ResultSet rs) throws SQLException {
+        int count = 0;
+        while (rs.next()) {
+            count = rs.getInt("COUNT(*)");
+        }
+        return count;
+    }
+}
